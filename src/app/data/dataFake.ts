f420621fdcf1efaf889de8d33afce48515ccb892
adevalter <adevalter@hotmail.com>
2023-05-01 21:32:49 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+    "id":"1",
+    "title": "NOVO HOME DE FERRO ANUNCIADO",
+    "description":"Marvel Anuncia nada de novo",
+    "photoCover":"https://www.magazine-hd.com/apps/wp/wp-content/uploads/2022/09/iron-man-ea.jpg"
+    },
+    {
+    "id":"2",
+    "title": "Novo Filme",
+    "description":"ola",
+    "photoCover":"https://upload.wikimedia.org/wikipedia/pt/d/d3/P%C3%B4ster_-_Marvel-Netflix.jpg"
+    }
+]
